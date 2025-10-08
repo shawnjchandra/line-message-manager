3dfd0598f6fd91b6cb19517960a5a2fa4ec97690
Shawn <shawnjchandra@gmail.com>
2025-10-08 23:40:32 +0700

    add: pages folder dan pages utama default

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './pages/Home'
+import MainTemplater from './pages/MainTemplater'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/templater">Templater</Link></li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/templater" component={MainTemplater}/>
+        </Switch>
+      </div>
+    </Router>
+   </>
   );
 }
 

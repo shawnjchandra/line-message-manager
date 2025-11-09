96f21a1faf43ab58f3da7d8b6f8469b8ddc1ffc5
Shawn <myking123321@gmail.com>
2025-11-09 18:58:05 +0700

    edit: edit untuk mengakomodasi penambahan translate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './pages/Home'
 import MainTemplater from './pages/MainTemplater'
 import Login from './pages/Login/Login';
-import './styles/custom.scss';
+import TranslationButton  from './components/TranslationButton/TranslationButton'
+import './utils/i18n'
 
 function App() {
   return (
    <>
     <Router>
       <div>
-        <nav>
-          <ul>
+        <nav >
+        
+          <ul style={{display:'flex'}}>    {/*  Hanya untuk nyoba saja, nanti hilangin semua inline styling*/}
             <li><Link to="/">Home</Link></li>
             <li><Link to="/templater">Editor</Link></li>
             <li><Link to="/login">Login</Link></li>
@@ -23,6 +24,7 @@ function App() {
           <Route path="/templater" component={MainTemplater}/>
           <Route path="/login" component={Login}/>
         </Switch>
+          <TranslationButton/>
       </div>
     </Router>
    </>

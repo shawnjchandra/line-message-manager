c6db172eab2623442d300177e2f078837751b183
ALEXANDER STANISLAUS BILIANTARA <alexanderbilliantara@gmail.com>
2025-11-13 15:03:41 +0700

    feat:updated custom navbar

--- frontend/src/components/Navbar/CustomeNavbar.tsx
+++ frontend/src/components/Navbar/CustomeNavbar.tsx
@@ -33,34 +33,3 @@ function CustomNavbar() {
 }
 
 export default CustomNavbar;
-
-// import react from 'react';
-// import { Link } from 'react-router-dom';
-// import farsLogo from '../assets/FARS_logo.png';
-// import '../styles/_navbar.scss';
-// import Container from 'react-bootstrap/Container';
-// import Navbar from 'react-bootstrap/Navbar';
-
-// const MyNavbar = () => {
-//     return (
-//         <nav className="custom-navbar">
-//             <div className="nav-left">
-//                 <div className="nav-logo">
-//                     <img src={farsLogo} className="fars-logo" />
-//                 </div>
-//                 <ul className="nav-links-main">
-//                     <li><Link to="/">Home</Link></li>
-//                     <li><Link to="/templater">Editor</Link></li>
-//                 </ul>
-//             </div>
-//             <div className="nav-right">
-//                 <ul className="nav-links-auth">
-//                     <li><Link to="/register">Sign Up</Link></li>
-//                     <li><Link to="/login">Login</Link></li>
-//                 </ul>
-//             </div>
-//         </nav>
-//     );
-// };
-
-// export default MyNavbar;

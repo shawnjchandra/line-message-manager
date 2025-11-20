880ce3ab4df89de0fd0a0bdaf9c8311205006993
Shawn <myking123321@gmail.com>
2025-11-20 13:57:20 +0700

    refactor: move and rename editor page and fix customnavmbar on app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,24 +3,24 @@ import Home from './pages/Home/Home'
 import MainTemplater from './pages/MainTemplater'
 import Login from './pages/Login/Login';
 import TranslationButton  from './components/TranslationButton/TranslationButton'
-import Editor from './pages/EditorPage';
+import Editor from './pages/AssetManager/AssetManager';
 
 import './utils/i18n'
 import CustomNavbar from './components/Navbar/CustomeNavbar';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
    <>
     <Router>
-      <div>
         <CustomNavbar/>
         <Switch>
           <Route exact path="/" component={Home}/>
           <Route path="/login" component={Login}/>
           <Route path="/editor" component={Editor} /> 
+          <Route path="/register" component={Register} /> 
         </Switch>
           <TranslationButton/>
-      </div>
     </Router>
    </>
   );

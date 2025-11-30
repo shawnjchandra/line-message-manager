5ce5096fb6684df5dbec29504b176daedb6b4043
Shawn <myking123321@gmail.com>
2025-11-30 21:58:54 +0700

    edit: allow

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); 
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // 1. Define the BASE directory where all data lives
 const DATA_DIR = path.join(__dirname, 'data');

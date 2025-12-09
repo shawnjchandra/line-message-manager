5f0d3ea406b14340210f4c2ef05788cc6f5fd86a
Shawn <myking123321@gmail.com>
2025-12-10 00:41:05 +0700

    edit:spline test to home

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,13 +2,25 @@ import farsLogo from "../../assets/FARS_logo.png";
 import "./Home.scss";
 import homebg from "../../assets/360_F_428871097_74QTIx9LRjJSBTnknjFlX2pSTRIL1edy.jpg"
 import { useTranslation } from "react-i18next";
+import Spline from '@splinetool/react-spline';
+import { Application } from "@splinetool/runtime";
+import { useEffect, useRef } from "react";
 
 function Home() {
     const { t } = useTranslation();
+  
+     const canvasRef = useRef(null);
 
-    return <>
-      <div className="home">
-        <div className="home-title-container">
+    useEffect(() => {
+      if (canvasRef.current) {
+        const app = new Application(canvasRef.current);
+        app.load('https://prod.spline.design/QRjbfCGmFaFhk6DO/scene.splinecode');
+      }
+    }, []);
+    return <canvas ref={canvasRef} />
+    // <>
+      {/* <div className="home"> */}
+        {/* <div className="home-title-container">
           <div className="home-titles">
             <div className="fars-logo-container">
               <img src={farsLogo} className="fars-center-logo"/>
@@ -18,9 +30,9 @@ function Home() {
           <div className="bg-container">
               <img src={homebg} className="background-img" />
           </div>
-        </div>
-      </div>
-    </>
+        </div> */}
+      {/* </div> */}
+    // </>
 }
 
 export default Home;
